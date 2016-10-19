ab90d366742660430305efa0a5f1dda84d3d5606
VladSafronov <artem.safronov923@gmail.com>
2016-10-19 16:52:20 +0300

    asdasdasdasdasd

--- app/src/main/java/com/max/timetable10/TabsActivity.java
+++ app/src/main/java/com/max/timetable10/TabsActivity.java
@@ -2,6 +2,7 @@ package com.max.timetable10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class TabsActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class TabsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
+        Log.d("asdasdasdas","sadasd");
     }
 }
